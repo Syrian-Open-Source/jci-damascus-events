209dfd4940159e5df1a99fcba5abb14472149bf0
karam  mustafa <karam2mustafa@gmail.com>
2022-03-03 20:37:17 +0200

    wip

--- app/Http/Controllers/Site/MenuController.php
+++ app/Http/Controllers/Site/MenuController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
@@ -18,6 +19,22 @@ class MenuController extends Controller
      */
     public function show(Event $event)
     {
+        return view('pages.menus.show' , ['data' => $event->load('menus' , 'menus.menuItems')->menus]);
+    }
+
+    /**
+     * show all menus inside specific event.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Menu  $event
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @author karam mustafa
+     */
+    public function save(Request $request, Menu $event)
+    {
+        dd($request->all());
+
         return view('pages.menus.show' , ['data' => $event->load('menus' , 'menus.menuItems')->menus]);
     }
 }
