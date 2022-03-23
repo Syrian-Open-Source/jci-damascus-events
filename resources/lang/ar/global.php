9d8ef6ec6c7998bea7e596559e0727c1fddb27f7
karam  mustafa <karam2mustafa@gmail.com>
2022-03-23 17:53:33 +0200

    wip

---
+++ resources/lang/ar/global.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'texts' => [
+        'event_introduction' => 'text text text text text text text text text text text text text text text text text text text text text text text text text  '
+    ],
+    'titles' => [
+        'event_introduction' => 'JCI Damascus Event System'
+    ],
+];
