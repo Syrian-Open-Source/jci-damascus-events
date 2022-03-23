7590c17304993eece508d4c13b6460c75d6f1054
karam  mustafa <karam2mustafa@gmail.com>
2022-03-23 17:59:33 +0200

    wip

--- resources/lang/en/global.php
+++ resources/lang/en/global.php
@@ -3,7 +3,12 @@
 
 return [
     'texts' => [
-        'event_introduction' => 'text text text text text text text text text text text text text text text text text text text text text text text text text  '
+        'event_introduction' => 'It is an JCI Damascus events management system.
+         The system displays the available events and then uses your personal information entered by the site administration to enter and interact with the activities related to each event,
+         including food reservations, table reservations and seating.
+         This system was developed by members of the technical team in JCI Damascus , in cooperation with the Syrian Open Source platform
+         You Can Follow the project in github from Here https://github.com/Syrian-Open-Source/jci-damascus-events
+         '
     ],
     'titles' => [
         'event_introduction' => 'JCI Damascus Event System'
