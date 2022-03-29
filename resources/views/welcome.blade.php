19d13b96473c6b66d3462da0d172d663fb56a0c8
karam  mustafa <karam2mustafa@gmail.com>
2022-03-30 02:12:39 +0300

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,9 @@
     @component('components.logo-center')@endcomponent
 
     <img class="w-100" src="{{asset('/images/event-background.jpg')}}" />
+
+    <div class="event-introduction text-center">
+        <h1> {{trans('global.titles.event_introduction')}} </h1>
+        <p> {{trans('global.texts.event_introduction')}} </p>
+    </div>
 @endsection
