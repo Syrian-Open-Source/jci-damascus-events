764498ec2151f085bca7dded34ac3559275e9560
karam  mustafa <karam2mustafa@gmail.com>
2022-03-30 20:02:04 +0300

    wip

--- resources/views/components/contribution-section.blade.php
+++ resources/views/components/contribution-section.blade.php
@@ -1,8 +1,15 @@
-<div class="contribution-section">
+<div class="contribution-section container mt-5">
     <div class="row w-100 h-100">
-        <div class="col-md-6 w-100 h-100">
+        <div class="col-md-6">
             <img src="{{asset('images/open-source.png')}}">
         </div>
-        <div class="col-md-6"></div>
+        <div class="col-md-6 mt-4">
+            <div class="event-introduction text-center w-100">
+                <h1 class="font-weight-bold"> {{trans('global.titles.event_introduction')}} </h1>
+                <p class="intro-p"> {{trans('global.texts.event_introduction')}} <a
+                        href='https://github.com/Syrian-Open-Source/jci-damascus-events'>{{trans('global.here')}}</a>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/components/logo-center.blade.php
+++ resources/views/components/logo-center.blade.php
@@ -1,3 +1,3 @@
-<div class="mt-5 logo-center">
+<div class="mt-5 logo-center position-relative">
  <img class="w-100 h-100" src="{{asset('images/logo.png')}}" >
 </div>
