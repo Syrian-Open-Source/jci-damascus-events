27ecf92496bd2de0c6cfafe5a00a315755062194
karam  mustafa <karam2mustafa@gmail.com>
2022-03-30 20:31:28 +0300

    wip

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="footer">
     <p class="text-center">{{trans('global.texts.footer')}}</p>
     <div class=" w-100 d-inline-flex justify-content-center align-items-center">
-        <a href="" class="mx-4"><img class="footer-icon" src=""></a>
-        <a href="" class="mx-4"><img class="footer-icon" src=""></a>
-        <a href="" class="mx-4"><img class="footer-icon" src=""></a>
+        <a href="" class="mx-4"><img class="footer-icon" src="{{asset('icons/facebook-icon.png')}}"></a>
+        <a href="" class="mx-4"><img class="footer-icon" src="{{asset('icons/insta-icon.png')}}"></a>
+        <a href="" class="mx-4"><img class="footer-icon" src="{{asset('icons/website-icon.png')}}"></a>
 
     </div>
 </div>
