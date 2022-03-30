49142cbb635d61726a5efa6d8e15dd8222358c54
karam  mustafa <karam2mustafa@gmail.com>
2022-03-30 18:48:54 +0300

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
     <div class="event-introduction text-center w-100">
         <h1 class="font-weight-bold"> {{trans('global.titles.event_introduction')}} </h1>
-        <p class="w-50 mx-auto"> {{trans('global.texts.event_introduction')}} <a href='https://github.com/Syrian-Open-Source/jci-damascus-events'>{{trans('global.here')}}</a></p>
+        <p class="mx-auto intro-p"> {{trans('global.texts.event_introduction')}} <a href='https://github.com/Syrian-Open-Source/jci-damascus-events'>{{trans('global.here')}}</a></p>
     </div>
 
     @component('components.event-cards')@endcomponent
