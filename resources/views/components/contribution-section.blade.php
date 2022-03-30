c7ab5b76274c7e227919bb2e1a9f8cb9098cb014
karam  mustafa <karam2mustafa@gmail.com>
2022-03-30 19:50:49 +0300

    wip

---
+++ resources/views/components/contribution-section.blade.php
@@ -0,0 +1,8 @@
+<div class="contribution-section">
+    <div class="row w-100 h-100">
+        <div class="col-md-6 w-100 h-100">
+            <img src="{{asset('images/open-source.png')}}">
+        </div>
+        <div class="col-md-6"></div>
+    </div>
+</div>
