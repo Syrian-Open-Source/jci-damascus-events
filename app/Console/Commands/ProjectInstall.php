524cd1b63351fac5c2c7aa5b37c1f5f41d01406c
karam  mustafa <karam2mustafa@gmail.com>
2022-04-06 23:15:40 +0300

    feat(command): add project install command

---
+++ app/Console/Commands/ProjectInstall.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ProjectInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jci:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        system('php artisan backpack:install');
+        $this->info('Backpack package has installed successfully');
+        system('php artisan vendor:publish --tag=blueprint-config');
+        $this->info('blueprint package has installed successfully');
+
+    }
+}
