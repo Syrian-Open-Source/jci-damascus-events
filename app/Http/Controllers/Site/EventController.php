dee5861657bf60ae08436a1c21e7d6e85fe8c247
karam  mustafa <karam2mustafa@gmail.com>
2022-04-08 00:59:53 +0300

    wip

---
+++ app/Http/Controllers/Site/EventController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+
+
+    public function index()
+    {
+        return view('pages.event', ['data' => Event::active()]);
+    }
+
+    public function show(Event $event)
+    {
+        return view('pages.event', ['data' => $event]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/events', [\App\Http\Controllers\Site\EventController::class, 'index'])->middleware(['auth','verified']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
