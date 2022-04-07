ce83939e3b25cf4b445537a7215965965da163ff
karam  mustafa <karam2mustafa@gmail.com>
2022-04-07 22:25:43 +0300

    wip

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,8 @@
     <p class="text-center text text-white p-3">{{trans('global.texts.footer')}}</p>
     <hr class="white-hr">
     <div class=" w-100 mt-3 d-inline-flex justify-content-center align-items-center">
-        <a href="" class="mx-4"><img class="footer-icon" src="{{asset('icons/facebook-icon.png')}}"></a>
-        <a href="" class="mx-4"><img class="footer-icon" src="{{asset('icons/insta-icon.png')}}"></a>
-        <a href="" class="mx-4"><img class="footer-icon" src="{{asset('icons/website-icon.png')}}"></a>
+        <a target="_blank" href="https://www.facebook.com/DamascusJCI" class="mx-4"><img class="footer-icon" src="{{asset('icons/facebook-icon.png')}}"></a>
+        <a target="_blank" href="https://instagram.com/jci_damascus" class="mx-4"><img class="footer-icon" src="{{asset('icons/insta-icon.png')}}"></a>
+        <a target="_blank" href="https://jci-damascus.org" class="mx-4"><img class="footer-icon" src="{{asset('icons/website-icon.png')}}"></a>
     </div>
 </div>
