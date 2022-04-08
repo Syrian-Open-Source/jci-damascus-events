420ae10ba6c54195459cd9ad3ef7981a7fe3bcc9
karam  mustafa <karam2mustafa@gmail.com>
2022-04-08 16:31:01 +0300

    wip

--- resources/views/pages/events/show.blade.php
+++ resources/views/pages/events/show.blade.php
@@ -8,9 +8,9 @@
                     <img class="card-img-top" src="{{asset('images/tables.webp')}}" alt="Card image cap">
                 </div>
                 <div class="food-introduction col-md-6 flex-column align-self-center">
-                    <h3>{{trans('global.titles.food_menu_text')}}</h3>
-                    <p>{{trans('global.texts.food_menu_text')}}</p>
-                    <button class="btn btn-outline-info">{{trans('global.buttons.menu')}}</button>
+                    <h3>{{trans('global.titles.table_text')}}</h3>
+                    <p>{{trans('global.texts.table_text')}}</p>
+                    <button class="btn btn-outline-info">{{trans('global.buttons.table')}}</button>
                 </div>
             </div>
             <div class="row flex-column-reverse flex-lg-row p-3">
