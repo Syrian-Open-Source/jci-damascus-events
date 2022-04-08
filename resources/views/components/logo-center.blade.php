dd1601052ee8ce4854cf2bb62e923b789b19bcb2
karam  mustafa <karam2mustafa@gmail.com>
2022-04-08 14:48:49 +0300

    wip

--- resources/views/components/logo-center.blade.php
+++ resources/views/components/logo-center.blade.php
@@ -1,3 +1,3 @@
 <div class="mt-5 logo-center position-relative">
- <img class="w-100 h-100" src="{{asset('images/logo.png')}}" >
+ <a href="/"><img class="w-100 h-100" src="{{asset('images/logo.png')}}" ></a>
 </div>
