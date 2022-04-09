efd7cfc72dc8bf6a314fc9aaf8c69050b2e0bbad
karam  mustafa <karam2mustafa@gmail.com>
2022-04-09 14:50:20 +0300

    wip

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,2 +1,5 @@
 @component('components.logo-center')@endcomponent
-<div class="text-center my-3">@component('components.auth-buttons')@endcomponent</div>
+<div class="text-center my-3">
+    @component('components.auth-buttons')@endcomponent
+    @component('components.lang-button')@endcomponent
+</div>
