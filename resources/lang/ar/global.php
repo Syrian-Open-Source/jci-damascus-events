6aba89a81ee678fcda9454b59ea27037c17c9207
karam  mustafa <karam2mustafa@gmail.com>
2022-04-09 13:00:13 +0300

    wip

--- resources/lang/ar/global.php
+++ resources/lang/ar/global.php
@@ -3,14 +3,57 @@
 
 return [
     'texts' => [
-        'event_introduction' => 'It is an JCI Damascus events management system.
-         The system displays the available events and then uses your personal information entered by the site administration to enter and interact with the activities related to each event,
-         including food reservations, table reservations and seating.
-         This system was developed by members of the technical team in JCI Damascus , in cooperation with the Syrian Open Source platform
-         You Can Follow the project in github from Here https://github.com/Syrian-Open-Source/jci-damascus-events
-         '
+        'event_introduction' => '
+        هو نظام إدارة الفعاليات التابع للغرفة الفتية  الدولية بدمشق.
+        طور فريق مبرمجين الغرفة الفتية الدولية دمشق  هذا النظام بالتعاون مع منصة Syrian Open Source للبرمجيات مفتوحة المصدر، يمكنك تسجيل حساب جديد على هذا النظام وعند موافقة مدير النظام تستطيع التفاعل مع الفعاليات التي تقيمها الغرفة الفتية الدولية دمشق والتسجيل على قوائم الطعام وأماكن الجلوس، يمكنك متابعة المشروع على موقع جيتهاب من خلال الرابط التالي
+         ',
+
+        'available_events_text' => "
+         The first step, you must have an account activated by an JCI damascus administration, on this system and you must be a member within JCI Damascus\n\n
+         This system is dedicated to providing technical solutions to members by facilitating some traditional operations\n\n
+         You can press the following button to log in and we will take you on a tour of the services within the system
+        ",
+
+        'footer' => 'It seems that you have already reached the end of the page,
+         do not forget to thank the efforts of the volunteers and programmers who contribute to jci projects and syrian open source,
+          yes we do many updates with volunteer efforts, and in order to be able to reach our goals to wider communities and implement more development projects,
+         do not forget to follow us on social media Through the following links',
+
+        'contributions_program_text' => 'One of the goals is to participate in social responsibility by launching open source projects for free. This program aims to provide opportunities for programmers who contribute to help build projects, gain experience and get to know developers from all over the world.',
+        'menu_text' => 'Through this feature, see the menu prepared for this event, you can choose the meals you like and choose even the allowed number of meals for the night, feel free to see the menu, it\'s simple and easy to choose',
+        'table_text' => 'Through this feature, you can see the tables at the event, you can also see the people who have reserved a chair to sit at a particular table, and if the table is still vacant, you can also reserve a seat there. Enjoy sitting next to someone you share ideas with and laughing',
+        'menu_description' => 'Here you can see all the items inside this menu, you can choose up to :count from this menu, notes: :notes ',
+        'edit_warning' => 'Please be careful note that you cannot edit after submitting',
+        'needs_approved' => 'Your account has not approved from the admin, if you think this was an error, please contact with the admin or support team',
     ],
     'titles' => [
-        'event_introduction' => 'JCI Damascus Event System'
+        'contributions_program' => 'برنامج الغرفة الفتية الدولية دمشق للمشاركة في البرمجيات المفتوحة المصدر',
+        'event_introduction' => 'نظام الغرفة الفتية الدولية دمشق لإدارة الفعاليات',
+        'available_events' => 'الفعاليات المتاحة',
+        'menu_text' => 'حجز مأكولات من قائمة الطعات',
+        'table_text' => 'حجز كرسي في طاولة',
     ],
+
+    'buttons' => [
+        'details' => 'المزيد من التفاصيل',
+        'login' => 'تسجيل الدخول',
+        'register' => 'حساب جديد',
+        'logout' => 'تسجيل الخروج',
+        'menu' => 'قائمة الطعام',
+        'table' => 'الطاولات المتاحة',
+        'show_menu_items' => 'قائمة الطعام',
+        'save' => 'حفظ',
+        'home' => 'الرئيسية',
+        'show_table_members' => 'رؤية أعضاء هذه الطاول',
+
+    ],
+
+    'here' => 'هنا',
+    'or_register' => 'تسجيل حساب جديد',
+    'title' => 'العنوان',
+    'description' => 'الوصف',
+    'choose' => 'اختيار',
+    'name' => 'الاسم',
+    'table_capacity_exceeded' => 'لا يمكنك التسجيل على هذه الطاول لانها وصلت الى الحد الاقصى من عمليات التسجيل',
+    'registered_before' => 'لقد قمت بالفعل بالستجيل على طاولة، لا يمكنك التسجيل على طاولة أخرى.',
 ];
