0033cda846dbec9eb5112616ae810931620f9a0e
karam  mustafa <karam2mustafa@gmail.com>
2022-04-09 14:55:41 +0300

    wip

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 @component('components.logo-center')@endcomponent
-<div class="text-center my-3">
-    @component('components.auth-buttons')@endcomponent
+<div class="my-3 d-flex justify-content-between w-25 mx-auto">
+     @component('components.auth-buttons')@endcomponent
     @component('components.lang-button')@endcomponent
 </div>
