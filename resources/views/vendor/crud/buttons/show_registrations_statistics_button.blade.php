8d5e217b1d4568bfac53c4c8351135e4246cba7d
karam  mustafa <karam2mustafa@gmail.com>
2022-04-22 22:20:59 +0300

    fix and refactor

--- resources/views/vendor/crud/buttons/show_registrations_statistics_button.blade.php
+++ resources/views/vendor/crud/buttons/show_registrations_statistics_button.blade.php
@@ -1,3 +1,5 @@
 @if ($crud->hasAccess('show'))
-    <a href="{{route('show_registrations_statistics' , $entry->getKey())}} " class="btn btn-xs btn-default"><i class="fa fa-ban"></i> {{trans('global.buttons.show_registrations_statistics')}}</a>
+    <a href="{{route('show_registrations_statistics' , $entry->getKey())}} " class="btn btn-xs btn-default">
+        <i class="fa fa-ban"></i> {{trans('global.buttons.show_registrations_statistics')}}
+    </a>
 @endif
